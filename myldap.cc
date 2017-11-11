f4064f310ea6c6b0103e074390ffb0762030991e
H00N24 <ondrej.kurak.h@gmail.com>
2017-11-11 17:29:54 +0100

    port, inp. file

--- myldap.cc
+++ myldap.cc
@@ -6,7 +6,23 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    
-    Server serv(PORT, "db.csv");
+    int opt = 0;
+    string file_name = "";
+    int port = 389;
+    while ((opt = getopt(argc, argv, "p:f:")) != -1) {
+        switch (opt) {
+            case 'p':
+                port = atoi(optarg);
+                break;
+            case 'f':
+                file_name = optarg;
+                break;
+            default:
+                cerr << "Arg error" << endl;
+                exit(0);
+        }
+    }
+
+    Server serv(port, file_name);
     serv.start();
 }
